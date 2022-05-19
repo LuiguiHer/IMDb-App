78e171152a44370bcf5c8d7c58c317e574cc3c7c
LuiguiHer <hernandez4389562@gmail.com>
2022-05-19 12:51:50 -0500

    icon-button, focus-inputUser/password, changeColor-BtnLogin

--- app/src/main/java/com/example/imdb/MainActivity.kt
+++ app/src/main/java/com/example/imdb/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.imdb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.imdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.inputPass.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus){
+                changeBtn()
+            }
+        }
+
+        binding.inputUser.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus ){
+                changeBtn()
+            }
+        }
+
     }
+
+    fun changeBtn (){
+        if(binding.inputUser.text.toString().isNotEmpty() || binding.inputPass.text.toString().isNotEmpty()){
+            binding.btnLogin.setBackgroundResource(R.drawable.button_secondary)}
+        else
+            binding.btnLogin.setBackgroundResource(R.drawable.button_principal)
+    }
+
+
 }
