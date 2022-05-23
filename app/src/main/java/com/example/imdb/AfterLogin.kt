98d12af20883a472028565f5aa638417a1379e9d
LuiguiHer <hernandez4389562@gmail.com>
2022-05-22 22:32:11 -0500

    new Activity created -AfterLogin

---
+++ app/src/main/java/com/example/imdb/AfterLogin.kt
@@ -0,0 +1,11 @@
+package com.example.imdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AfterLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_login)
+    }
+}
